5181113c8b2d9144dd370e0fa8b1b86a93184b06
isa <isaliangyiyun@hotmail.com>
2017-01-09 07:13:20 -0500

    add Link from react-router

--- app/app.jsx
+++ app/app.jsx
@@ -12,11 +12,13 @@ var {name} = obj;
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
+var Examples = require('Examples');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <Route path='about' component={About}></Route>
+      <Route path='examples' component={Examples}></Route>
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
