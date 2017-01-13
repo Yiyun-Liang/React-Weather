9d128946cd1d38143128811fb6c54850d8b9795c
isa <isaliangyiyun@hotmail.com>
2017-01-12 20:16:34 -0500

    add loading text

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -7,31 +7,48 @@ var Weather = React.createClass({
   // default when app loads, below is a built in function
   getInitialState: function(){
     return {
-      location: 'Seattle',
-      temp: 28
+      isLoading: false
     }
   },
   handleSearch: function(location){
     var that = this;
 
+    this.setState({
+      isLoading: true
+    });
+
     openWeatherMap.getTemp(location).then(function(temp){
       // simply using this here not working - this binding is lost inside here...
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function(errMsg){
+      that.setState({
+        isLoading: false
+      });
       alert(errMsg);
     });
   },
   render: function(){
-    var {location, temp} = this.state;
+    // ES6 destructuring
+    var {isLoading, location, temp} = this.state;
+
+    // conditionally renders components
+    function renderMessage(){
+      if(isLoading){
+        return <h3>Fetching weather...</h3>
+      }else if(temp && location){
+        return <WeatherMessage location={location} temp={temp}/>
+      }
+    }
 
     return(
       <div>
         <h1>Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage location={location} temp={temp}/>
+        {renderMessage()}
       </div>
     );
   }
