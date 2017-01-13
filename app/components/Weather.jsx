fac80f0954e0616bee9bbe9a79653f300aaaa617
isa <isaliangyiyun@hotmail.com>
2017-01-12 19:28:33 -0500

    data flow-handle browser action(form submited)

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,11 +3,14 @@ var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
+  handleSearch: function(location){
+    alert(location);
+  },
   render: function(){
     return(
       <div>
         <h1>Get Weather</h1>
-        <WeatherForm/>
+        <WeatherForm onSearch={this.handleSearch}/>
         <WeatherMessage/>
       </div>
     );
