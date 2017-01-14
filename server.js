5760adb80b49b0a6836a121ffae6c28348d8dc28
isa <isaliangyiyun@hotmail.com>
2017-01-13 21:39:26 -0500

    fix localhost issue

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 // express middleware: do something with every request
 // this needs to be above app.use(express.static)
+// this may break localhost if if and else code are swapped
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next(); // let request process as normal in this case
-  }else{
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  }else{
+    next(); // let request process as normal in this case
   }
 });
 
