1f651df8be60207713475ca9718da6e89fadfd21
isa <isaliangyiyun@hotmail.com>
2017-01-16 20:51:00 -0500

    fix error modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 //facebook.github.io/react/docs/component-specs.html
 var ErrorModal = React.createClass({
@@ -12,18 +14,34 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
   componentDidMount: function(){
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function(){
     var {title, message} = this.props;
-    return(
+
+    var modalMarkup = (
       <div id='error-modal' className='reveal tiny text-center' data-reveal>
         <h4>{title}</h4>
         <p>{message}</p>
         <p>
           <button className='button' data-close=''>Okay</button>
         </p>
+      </div>
+    );
+
+    // add this markup into DOM
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup)); //convert to string
+    // add to DOM
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function(){
+
+    // minimum
+    // start with no DOM
+    // since foundation tries to manipulate with DOM when calling modal.open()
+    // react does not work well with third party library updating dom like that
+    return(
+      <div>
       </div>
     );
   }
